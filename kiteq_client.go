8608460cbda67db1144d5081226cff7f23ee6af0
zhao.chao <zhao.chao@uneed.com>
2020-11-05 11:40:09 +0800

    增加了mock的kiteqclient

--- kiteq_client.go
+++ kiteq_client.go
@@ -6,7 +6,8 @@ import (
 )
 
 type KiteQClient struct {
-	k *kite
+	mockModel bool //mock的模式
+	k         *kite
 }
 
 func (self *KiteQClient) Start() {
@@ -26,40 +27,64 @@ func NewKiteQClient(zkAddr, groupId, secretKey string) *KiteQClient {
 	return NewKiteQClientWithWarmup(zkAddr, groupId, secretKey, 0)
 }
 
+//mock的kiteQClient
+func NewMockKiteQClient(zkAddr, groupId, secretKey string) *KiteQClient {
+	return &KiteQClient{k: &kite{}, mockModel: true}
+}
+
 func NewKiteQClientWithWarmup(zkAddr, groupId, secretKey string, warmingupSec int) *KiteQClient {
 	return &KiteQClient{
 		k: newKite(zkAddr, groupId, secretKey, warmingupSec, NewKiteQListener())}
 }
 
 func (self *KiteQClient) SetTopics(topics []string) {
-	self.k.SetPublishTopics(topics)
+	if !self.mockModel {
+		self.k.SetPublishTopics(topics)
+	}
 }
 
 func (self *KiteQClient) SetBindings(bindings []*registry.Binding) {
+	if self.mockModel {
+		return
+	}
 	self.k.SetBindings(bindings)
-
 }
 
 func (self *KiteQClient) SendTxStringMessage(msg *protocol.StringMessage, transcation DoTransaction) error {
+	if self.mockModel {
+		return nil
+	}
 	message := protocol.NewQMessage(msg)
 	return self.k.SendTxMessage(message, transcation)
 }
 
 func (self *KiteQClient) SendTxBytesMessage(msg *protocol.BytesMessage, transcation DoTransaction) error {
+	if self.mockModel {
+		return nil
+	}
 	message := protocol.NewQMessage(msg)
 	return self.k.SendTxMessage(message, transcation)
 }
 
 func (self *KiteQClient) SendStringMessage(msg *protocol.StringMessage) error {
+	if self.mockModel {
+		return nil
+	}
 	message := protocol.NewQMessage(msg)
 	return self.k.SendMessage(message)
 }
 
 func (self *KiteQClient) SendBytesMessage(msg *protocol.BytesMessage) error {
+	if self.mockModel {
+		return nil
+	}
 	message := protocol.NewQMessage(msg)
 	return self.k.SendMessage(message)
 }
 
 func (self *KiteQClient) Destory() {
+	if self.mockModel {
+		return
+	}
 	self.k.Destroy()
 }
